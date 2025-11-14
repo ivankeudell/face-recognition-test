062c48335a664a8f675bd27da839da6da8207a18
Iván Keudell <ivankeudell@gmail.com>
2025-11-14 10:37:04 -0300

    funcionalidad básica para poder subir y ver una foto

--- main.py
+++ main.py
@@ -1,7 +1,72 @@
-from flask import Flask
+from flask import Flask, Response, request
+import uuid
+
+IMAGES_ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp'}
 
 flask_server = Flask(__name__)
 
+uploaded_files = {}
+
+def get_file_extension(filename):
+	return filename.rsplit('.', 1)[1].lower()
+
+def is_file_extension_allowed(filename):
+	return '.' in filename and get_file_extension(filename) in IMAGES_ALLOWED_EXTENSIONS
+
+
 @flask_server.route('/')
-def hello_world():
+def root():
 		return flask_server.redirect("/static/index.html")
+
+
+
+@flask_server.route('/api/facial_auth', methods=['POST'])
+def facial_auth():
+	if request.method != 'POST':
+		return {"ok": False, "error": "Only POST method supported"}
+	
+	# Check if the request has the file part
+	if 'image-document' not in request.files \
+	or 'image-selfie' not in request.files:
+		return {"ok": False, "error": "Files were not sent"}
+
+	image_document = request.files['image-document']
+	image_selfie = request.files['image-selfie']
+
+	# If no files are sent, the browser sends an empty file with an empty filename
+	if image_document.filename == '' \
+	or image_selfie.filename == '':
+		return {"ok": False, "error": "Images were not sent"}
+	
+	if not is_file_extension_allowed(image_document.filename) \
+	or not is_file_extension_allowed(image_selfie.filename):
+		return {"ok": False, "error": "File not allowed"}
+
+	image_document_key = str(uuid.uuid4())
+	image_selfie_key = str(uuid.uuid4())
+
+	print("document key: " + image_document_key)
+	print("selfie key: " + image_selfie_key)
+
+	image_document_blob = image_document.read()
+	image_selfie_blob = image_selfie.read()
+
+	uploaded_files[image_document_key] = {
+		"filename": image_document.filename,
+		"blob": image_document_blob
+	}
+	uploaded_files[image_selfie_key] = {
+		"filename": image_selfie.filename,
+		"blob": image_selfie_blob
+	}
+
+	return {"ok": True}
+
+
+
+@flask_server.route('/api/seeUploads', methods=['GET'])
+def see_uploads():
+	image_key = request.args.get('key', '')
+	image = uploaded_files[image_key]["blob"]
+	mimetype = get_file_extension(uploaded_files[image_key]["filename"])
+	return Response(response=image, mimetype=mimetype)
