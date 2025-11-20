ddf67bbac595745c23057b5463fa9bf7a3da6868
Iván Keudell <ivankeudell@gmail.com>
2025-11-20 07:24:13 -0300

    cambiar entre las dos cámaras

--- static/main.js
+++ static/main.js
@@ -1,24 +1,105 @@
 let documentImageBlob;
+let selfieImageBlob;
 
-document.getElementById('document-btn').addEventListener('click', async function()
+let videoElement;
+let videoStream;
+
+async function createCameraFeed(useSelfieCamera)
 {
-	const video = document.getElementById('document-video');
-	const canvas = document.getElementById('document-canvas');
-	const ctx = canvas.getContext('2d');
-	const overlayImg = new Image();
-	overlayImg.src = "overlay_document.svg";
-	
+	const video = document.getElementById('video');
+
 	const stream = await navigator.mediaDevices.getUserMedia
 	({
 		video:
 		{
-			facingMode: 'environment',
+			facingMode: useSelfieCamera ? 'user' : 'environment',
 			resizeMode: 'crop-and-scale'
 		},
 		audio: false
 	});
 
+	videoStream = stream;
 	video.srcObject = stream;
+	video.play();
+	return video;
+}
+
+async function getCameraFeed()
+{
+	if(videoElement === undefined) videoElement = await createCameraFeed();
+	return videoElement;
+}
+
+function stopCameras()
+{
+	if(videoStream === undefined) throw new Error("Theres not a video stream to stop");
+	videoStream.getTracks().forEach(function(track)
+	{
+		track.stop();
+	});
+}
+
+async function changeCameraDirection(selfie)
+{
+	if(videoStream === undefined) throw new Error("Theres not a video stream available to change direction to");
+	stopCameras();
+
+	videoElement = await createCameraFeed(selfie);
+	return videoElement;
+}
+
+document.getElementById('document-btn').addEventListener('click', async function()
+{
+	const canvas = document.getElementById('document-canvas');
+	const ctx = canvas.getContext('2d');
+	const overlayImg = new Image();
+	overlayImg.src = "overlay_document.svg";
+
+	let video = await getCameraFeed();
+
+	function drawFrame()
+	{
+		let aspect = video.videoHeight / video.videoWidth;
+		let wantedWidth = window.innerWidth * 0.75;
+		let height = Math.round(wantedWidth * aspect);
+
+		//console.log(video.videoWidth, video.videoHeight, aspect, wantedWidth, height);
+	
+		canvas.width = wantedWidth;
+		canvas.height = height;
+
+		ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+		ctx.drawImage(overlayImg, 0, 0, canvas.width, canvas.height);
+
+		requestAnimationFrame(drawFrame);
+	}
+	
+	document.getElementById('document-section').hidden = true;
+	document.getElementById('document-canvas-container').hidden = false;
+
+	document.getElementById('document-shutter').addEventListener('click', async function()
+	{
+		documentImageBlob = await captureImage(video);
+		
+		let urlCreator = window.URL || window.webkitURL;
+		let imageUrl = urlCreator.createObjectURL(documentImageBlob);
+		document.getElementById('document-result').src = imageUrl;
+
+		document.getElementById('document-canvas-container').hidden = true;
+		document.getElementById('selfie-section').hidden = false;
+	});
+	//resize();
+	drawFrame();
+});
+
+document.getElementById('selfie-btn').addEventListener('click', async function()
+{
+	useSelfieCamera = true;
+
+	const canvas = document.getElementById('selfie-canvas');
+	const ctx = canvas.getContext('2d');
+
+	let video = await changeCameraDirection(true);
 
 	function drawFrame()
 	{
@@ -32,26 +113,30 @@ document.getElementById('document-btn').addEventListener('click', async function
 		canvas.height = height;
 
 		ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-		ctx.drawImage(overlayImg, 0, 0, canvas.width, canvas.height);
+		//ctx.drawImage(overlayImg, 0, 0, canvas.width, canvas.height);
 
 		requestAnimationFrame(drawFrame);
 	}
 	
-	document.getElementById('document-section').hidden = true;
+	document.getElementById('selfie-section').hidden = true;
+	document.getElementById('selfie-canvas-container').hidden = false;
 
-	document.getElementById('document-shutter').addEventListener('click', async function()
+	document.getElementById('selfie-shutter').addEventListener('click', async function()
 	{
-		documentImageBlob = await captureImage(video);
+		selfieImageBlob = await captureImage(video);
 		
 		let urlCreator = window.URL || window.webkitURL;
-		let imageUrl = urlCreator.createObjectURL(documentImageBlob);
-		document.getElementById('document-result').src = imageUrl;
+		let imageUrl = urlCreator.createObjectURL(selfieImageBlob);
+		document.getElementById('selfie-result').src = imageUrl;
+
+		document.getElementById('selfie-canvas-container').hidden = true;
+		document.getElementById('results').hidden = false;
+		stopCameras();
 	});
 	//resize();
-	video.play();
 	drawFrame();
-	
 });
+
 
 async function captureImage(video)
 {
@@ -67,16 +152,3 @@ async function captureImage(video)
 	});
 }
 
-/*
-function resize()
-{
-	let video = document.getElementById('document-video');
-	let canvas = document.getElementById('document-canvas');
-	video.style.height = window.innerHeight;
-	video.style.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	canvas.width = window.innerWidth;
-}
-
-window.addEventListener('resize', resize);
-*/
